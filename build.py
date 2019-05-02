7f9393d18fb706c908f4c3c007c7c6de5aa0927f
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-02 18:50:14 -0500

    Only do Release builds

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(build_types=["Release"], build_policy="outdated")
     builder.add_common_builds()
     builder.run()
